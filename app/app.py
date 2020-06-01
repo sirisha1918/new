de12d19c3bb999277be5ab1a77fc4274f76ebcb2
Salim <salim0630@gmail.com>
2020-05-31 23:24:45 -0400

    dockerized

--- app/app.py
+++ app/app.py
@@ -9,18 +9,25 @@ metadata_flavor_header = {"Metadata-Flavor": "Google"}
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def main():
-    return_resp = {}
-
-    for metadata_entry in metadata_entries_to_grab:
-        req = "{}{}".format(instance_metadata_url, metadata_entry)
-        resp = requests.get(req, headers=metadata_flavor_header)
-        return_resp[metadata_entries_to_grab[metadata_entry]] = resp.text
-
-    print(return_resp)
+    return_resp = {"message": "hello world"}
     return jsonify(return_resp)
 
+
+# @app.route('/')
+# def main():
+#     return_resp = {}
+#
+#     for metadata_entry in metadata_entries_to_grab:
+#         req = "{}{}".format(instance_metadata_url, metadata_entry)
+#         resp = requests.get(req, headers=metadata_flavor_header)
+#         return_resp[metadata_entries_to_grab[metadata_entry]] = resp.text
+#
+#     print(return_resp)
+#     return jsonify(return_resp)
+#
 
 # if __name__ == '__main__':
 #     app.run(host="0.0.0.0", port=80)
